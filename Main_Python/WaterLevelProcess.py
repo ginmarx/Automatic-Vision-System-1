630836cfc4725028f529709006e7d4944b50cb95
diceido <37260680+diceido@users.noreply.github.com>
2021-01-23 15:54:10 +0700

    the new one

--- Main_Python/WaterLevelProcess.py
+++ Main_Python/WaterLevelProcess.py
@@ -7,33 +7,33 @@ import numpy as np
 import pathlib
 
 def preprocessing(img):
-    img1 = img[0: 100, :]
+    img1 = img[0: 350, :]
     _, img1 = cv2.threshold(img1, 50, 255, cv2.THRESH_TOZERO)
     _, img1 = cv2.threshold(img1, 50, 255, cv2.THRESH_BINARY)
-    kernel = np.ones((5, 5), np.uint8)
+    kernel = np.ones((9, 9), np.uint8)
     img1 = cv2.morphologyEx(img1, cv2.MORPH_CLOSE, kernel)
-    img1 = cv2.erode(img1, (5, 5), iterations=1)
+    img1 = cv2.erode(img1, (7, 7), iterations=1)
     img1 = cv2.morphologyEx(img1, cv2.MORPH_OPEN, kernel)
 
-    img2 = img[100:130, :]
+    img2 = img[350:450, :]
     img2 = cv2.GaussianBlur(img2, (5, 5), np.sqrt(16))
     _, img2 = cv2.threshold(img2, 1, 255, cv2.THRESH_BINARY)
-    img2 = cv2.morphologyEx(img2, cv2.MORPH_CLOSE, kernel)
-    img2 = cv2.erode(img2, (13, 13), iterations=1)
-    img2 = cv2.morphologyEx(img2, cv2.MORPH_OPEN, kernel)
 
-    img3 = img[130::, :]
+
+    img3 = img[450::, :]
+    kernel1 = np.ones((5, 5), np.uint8)
     _, img3 = cv2.threshold(img3, 50, 255, cv2.THRESH_BINARY)
-    img3 = cv2.morphologyEx(img3, cv2.MORPH_CLOSE, kernel)
-    img3 = cv2.erode(img3, (5, 5), iterations=1)
-    img3 = cv2.morphologyEx(img3, cv2.MORPH_OPEN, kernel)
+    img3 = cv2.morphologyEx(img3, cv2.MORPH_CLOSE, kernel1)
+    img3 = cv2.erode(img3, (7, 7), iterations=3)
+    img3 = cv2.morphologyEx(img3, cv2.MORPH_OPEN, kernel1)
 
     hold = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
-    hold[0:100, :] = img1
-    hold[100:130, :] = img2
-    hold[130::, :] = img3
+    hold[0:350, :] = img1
+    hold[350:450, :] = img2
+    hold[450::, :] = img3
     hold = imu.auto_canny(hold)
     return hold
+
 def detect_lib(edge):
     cnts = cv2.findContours(edge, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imu.grab_contours(cnts)
@@ -85,6 +85,7 @@ def detect_waterlevel(edge):
             return box1
     else:
         return None
+
 def take4point(pc):
     # Sắp xếp các điểm có từ nhỏ đến lớn theo giá trị của trụ x trong ma trận
     xSorted = pc[np.argsort(pc[:, 0]), :]
@@ -113,11 +114,13 @@ def water_level_val(pt1, pt2, mid_ptX, mid_ptY):
     return level_val_cm
 def WaterLevelProcess(img,count,path):
 
-    img = img[350:800, 960:1500]
-    img = imu.resize(img, width=200)
-    edge = preprocessing(img)
-    box_of_lid = detect_lib(edge)
+    img = img[400:980, 828:1828]
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_gray = img_gray[400:980, 828:1828]
+    edge = preprocessing(img_gray)
+    edge = 2 * edge
     box_of_waterlevel = detect_waterlevel(edge)
+    box_of_lid = detect_lib(edge)
     if box_of_lid is not None:
         [tll, bll, brl, trl] = take4point(box_of_lid)
         pt1 = []
